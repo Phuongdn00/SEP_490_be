c1a66cceca14a2e4c24d57488a316bfa03fa9c9d
Phuongdn00 <100327758+Phuongdn00@users.noreply.github.com>
2023-05-02 10:29:40 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -10,10 +10,10 @@
 const mysql= require("mysql2/promise")
 
 const connection= mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "quan_li_thu_vien",
+    host: "ql479.main-hosting.eu",
+    user: "u291392387_root10",
+    password: "abc12345!A",
+    database: "u291392387_library_system",
     port:4306
 })
 
