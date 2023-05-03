b4314a9f573738939bef48b94f0f8e0e3fbc9307
Phuongdn00 <100327758+Phuongdn00@users.noreply.github.com>
2023-05-03 19:59:03 +0700

    committtttttt

--- controller/search.js
+++ controller/search.js
@@ -6,13 +6,14 @@ const options = {
     // Define an array of keys to be used in the search
     keys: [
       "book_name",
-      "author_name"
+      "author_name",
+      "category_name"
     ]
   };
   
 const search= expressAsyncHandler(async (req, res)=> {
     // Perform a join operation across the book, category_book, category, and author tables
-    const [rows]= await connection.execute("SELECT * FROM category_book INNER JOIN book ON book.book_id = category_book.book_id INNER JOIN category ON category.category_id = category_book.category_id INNER JOIN author ON author.author_id = book.author_id")
+    const [rows]= await connection.execute("SELECT DISTINCT * FROM category_book INNER JOIN book ON book.book_id = category_book.book_id INNER JOIN category ON category.category_id = category_book.category_id INNER JOIN author ON author.author_id = book.author_id")
     // Create a new instance of Fuse using the search keys defined in the options object
     const fuse = new Fuse(rows, options);
     
