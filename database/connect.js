4a347943b8bc6a2820ada4d70d71ba1008144545
Phuongdn00 <100327758+Phuongdn00@users.noreply.github.com>
2023-05-03 14:25:02 +0700

    fix35

--- database/connect.js
+++ database/connect.js
@@ -1,23 +1,14 @@
-// const { Sequelize } = require('sequelize');
-
-// const sequelize= new Sequelize({
-//     host: "localhost",
-//     username: "root",
-//     password: "",
-//     database: "quan_ly_thu_vien"
-// })
-
 const mysql= require("mysql2/promise")
 
 const connection= mysql.createPool({
     host: "151.106.124.151",
-     user: "u291392387_root10",
-    password: "GIangvippro1!",
+    user: "u291392387_root10",
+    password: "123456Aaa!",
     database: "u291392387_library_system",
     // host: "localhost",
-    //user: "root",
+    // user: "root",
     // password: "",
-    //database: "quan_li_thu_vien",
+    // database: "quan_li_thu_vien",
 })
 
 module.exports= connection
--- utils/mail.js
+++ utils/mail.js
@@ -1,18 +1,19 @@
 const nodemailer= require("nodemailer")
 const moment= require("moment")
 
+// create nodemailer transporter object with gmail service and authentication
 const transporter= nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "datistpham@gmail.com",
-        pass: "ekicxahksexlcegn"
+        user: "thuvienfudn@gmail.com",
+        pass: "wyxiaiqdiyzgxlfg"
     }
 })
 
-
-
+// define function to send verification email
 const verifyMail= async (email, code)=> {
     try {
+        // send email with specified content to specified email address
         const result= await transporter.sendMail({from: process.env.EMAIL_PROVIDER, to: email, subject: "Verify your email", text: "Your code is: "+ code})
         return result
         
